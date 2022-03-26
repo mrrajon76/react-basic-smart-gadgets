9bd9462cc84e19c6d742ac10c023ab95482a2fb5
Md. Rasheduzzaman Rajon <mrrajon76@gmail.com>
2022-03-26 06:47:10 +0600

    data fetched using useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
-// import Product from './components/Products/Product';
+import Product from './components/Products/Product';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Product></Product>
     </div>
   );
 }
